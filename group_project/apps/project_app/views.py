f364fe0ea51c8ab397ba6629f36014e817e9b8ac
Jason Lu <jasonlu121@gmail.com>
2018-10-24 10:08:50 -0700

    no idea just updating

--- group_project/apps/project_app/views.py
+++ group_project/apps/project_app/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
 from apps.project_app.models import User
 import bcrypt
+import argparse
+import json
+import pprint
+import requests
+import sys
+import urllib
+
+API_HOST = 'https://api.yelp.com'
+URL = 'https://api.yelp.com/v3/businesses/search'
+SEARCH_PATH = '/v3/businesses/search'
+api_key = '8fJisUcWi6_6M8q1TqXwV64duaoO7p6rs5Sh4xI9b6abzOxLgAHFW_OrD2jgX7rRH0a2bwm4Uhio4-5JiVQCbTHyvrzs8667unV_strpWIR6xq-CLwuT5V-uBH3KW3Yx'
+header = {'Authorization': 'Bearer ' + api_key}
+
+
 
 def index(request):
     if 'id' not in request.session or request.session['id'] == None:
@@ -49,22 +63,66 @@ def validate_login(request):
 def wheel(request):
     return render(request, "project_app/wheel.html")
 
+def process_wheel(request):
+    return redirect("/results")
+
 def preferences(request):
     return render(request, "project_app/preferences.html")
 
 def process_preferences(request):
+    request.session['category'] = request.POST['category']
+    request.session['price'] = request.POST['price']
+    request.session['city'] = request.POST["city"]
     return redirect('/wheel')
+
+def results(request):
+    print(request.session['data'])
+    return render(request, "project_app/testsubject.html")
 
 def success(request):
     data = User.objects.get(id=request.session['id'])
     userdict = {
         "datakey": data
-        
     }
-    print('helllooooooooooooooooo')
     return render(request, "project_app/success.html", userdict)
 
 def logout(request):
     request.session['id'] = None
     return redirect('/')
 
+def yelpAPI(request):
+    category = 'term=chinese'
+    location = 'location=Seattle'
+    pricepoint = 'price=2'
+    limit = 'limit=12'
+    rating = 'sort_by=rating'
+    response = requests.get(URL + '?{}&{}&{}&{}&{}'.format(category, location, pricepoint, limit, rating), headers = header)
+    # request.session['business'] = response.json()
+    business = response.json()
+    result = json.dumps(business, sort_keys=True, indent=4)
+    restdict = json.loads(result)
+    print("*"*80)
+    print(URL + '?{}&{}&{}&{}&{}'.format(category, location, pricepoint, limit, rating))
+    print(result)
+    print("*"*80)
+
+    # business = request.session['business']
+
+    # context = {
+    #     'mileradius' : business['radius'],
+    #     'location' : business['location'],
+    #     'latitude': business['latitude'],
+    #     'longitude': business['longitude'],
+    #     'phone' : business['phone'],
+    #     'url' : business['url'],
+    #     'rating' : business[ 'rating'],
+    #     'review_count' : business[ 'review_count'],
+    #     'price' : business['price'],
+    #     'name': business['name'],
+    #     'categories': business['categories'],
+    #     'is_cached': is_cached,
+    #     'api_key': 'AIzaSyCX4x-GRqo8LUQQyYnCy6rgmC5PsefMtes',  # Don't do this! This is just an example. Secure your keys properly.\
+    # }
+
+    # return render(request, 'project_app/testsubject.html')
+    return HttpResponse(result, content_type="application/json")
